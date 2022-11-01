45eaef45aafedfb05adbdd3ad0c470b2e170082b
Evan <evan.johnson99@yahoo.com>
2022-11-01 03:27:20 -0500

    added fname lname

--- db/migrate/20221101050052_create_attendants.rb
+++ db/migrate/20221101050052_create_attendants.rb
@@ -1,7 +1,8 @@
 class CreateAttendants < ActiveRecord::Migration[7.0]
   def change
     create_table :attendants do |t|
-      t.string :name
+      t.string :fname
+      t.string :lname
       t.string :email
 
       t.timestamps
